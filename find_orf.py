63ae91e0f9ed474034c903dd0e46946989a83a91
akilaabesinghe <akilaabesinghe@gmail.com>
2022-04-06 21:34:31 -0700

    regex

--- find_orf.py
+++ find_orf.py
@@ -57,17 +57,17 @@ def vet_nucleotide_sequence(sequence):
     # any valid RNA and DNA sequence strings, respectively (and only strings of
     # RNA and DNA bases).
     # Read the docstring above for additional clues.
-    rna_pattern_str = r'AUCG'
-    dna_pattern_str = r'ATCG'
+    rna_pattern_str = r'^[AUGC]*$'
+    dna_pattern_str = r'^[ATGC]*$'
     ##########################################################################
 
-    rna_pattern = re.compile(rna_pattern_str)
-    dna_pattern = re.compile(dna_pattern_str)
+    rna_pattern = re.compile(rna_pattern_str,re.IGNORECASE)
+    dna_pattern = re.compile(dna_pattern_str,re.IGNORECASE)
 
     if rna_pattern.match(sequence):
         return
     if dna_pattern.match(sequence):
-        return
+        return 
     else:
         raise Exception("Invalid sequence: {0!r}".format(sequence))
 
@@ -119,10 +119,10 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'AUG'
+    codon_pattern_str = r'^[ACGU]{3}$'
     ##########################################################################
 
-    codon_pattern = re.compile(codon_pattern_str)
+    codon_pattern = re.compile(codon_pattern_str,re.IGNORECASE)
 
     if codon_pattern.match(codon):
         return
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'(' + '|'.join(start_codons) + ')([AUGC]{3})*(' + '|'.join(stop_codons) + ')'
     ##########################################################################
 
     # Create the regular expression object
